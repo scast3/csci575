cd810ae4675aa43f7dfbf6366d3153669c79af1b
Jack Brennan <jackbrennan222@gmail.com>
2025-04-30 17:43:04 -0600

    model weights downloaded

--- sketch.js
+++ sketch.js
@@ -15,21 +15,8 @@ let boxCoords = {
   sHeight: 0
 }
 
-let imgs = [];
-
-let writer;
-
 function preload() {
   handPose = ml5.handPose({ flipped: true });
-
-  loadStrings("strings.txt", (arr) => {
-    for (let filename of arr.slice(0,5)) {
-      loadImage(filename, (img) => {
-        imgs.push({ img, label: filename.split("/")[1] })
-        console.log(filename.split("/"));
-      })
-    }
-  })
 }
 
 function setup() {
@@ -38,22 +25,7 @@ function setup() {
   video.size(videoSize, videoSize);
   video.hide();
 
-  writer = createWriter("test.txt");
-  for (let img of imgs) {
-    handPose.detect(img.img, (results) => {
-      if (results.length > 0) {
-        let inputs = [];
-        for (let kp of results[0].keypoints) {
-          inputs.push(kp.x);
-          inputs.push(kp.y);
-        }
-        console.log(inputs);
-        writer.print(img.label + " " + inputs);
-      }
-    })
-  }
-
-let options = {
+  let options = {
     inputs: 42,
     outputs: 27,
     task: "classification",
@@ -62,12 +34,43 @@ let options = {
   model = ml5.neuralNetwork(options);
 
   handPose.detectStart(video, gotHands);
-}
 
-function keyPressed() {
-  if (keyCode === ENTER) {
-    writer.close();
-  }
+  button = createButton("data");
+  button.mouseClicked(() => {
+    for (let c of ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Blank"]) {
+      loadStrings("inputs-"+c+".txt", (strings) => {
+        for (let s of strings) {
+          if (s === "") {
+            continue;
+          }
+          console.log(s);
+          let input = [];
+          for (let n of s.split(" ")[1].split(",")) {
+            input.push(parseFloat(n));
+          }
+          model.addData(input, {label: c});
+        }
+      })
+    }
+  });
+
+  b = createButton("Train");
+  b.mouseClicked(() => { 
+    console.log("Training");
+    model.normalizeData();
+    model.train({
+      epochs: 50,
+      batchSize: 100
+    }, () => {
+      console.log("Done");
+    });
+  });
+
+  b1 = createButton("Save");
+  b1.mouseClicked(() => { 
+    console.log("Saving");
+    model.save();
+  });
 }
 
 function gotHands(results) {
@@ -100,52 +103,53 @@ function videoReady() {
   ready = true;
 }
 
-// function draw() {
-//   background(255);
+function draw() {
+  background(255);
 
-//   if (ready) {
-//     image(video, 0, 0);
-//   }
+  if (ready) {
+    image(video, 0, 0);
+  }
 
-//   if (hands.length > 0) {
-//     for (let hand of hands) {
-//       if (hand.confidence > 0.1) {
-//         if (hand.handedness == "Left") {
-//           fill(255, 0, 255);
-//         } else {
-//           fill(255, 255, 0);
-//         }
+  if (hands.length > 0) {
+    for (let hand of hands) {
+      if (hand.confidence > 0.1) {
+        if (hand.handedness == "Left") {
+          fill(255, 0, 255);
+        } else {
+          fill(255, 255, 0);
+        }
         
-//         for (let i = 0; i < hand.keypoints.length; i++) {
-//           let keypoint = hand.keypoints[i];
-//           circle(keypoint.x, keypoint.y, 16);
-//           boxCoords = {
-//             sx: min(boxCoords.sx, keypoint.x),
-//             sy: min(boxCoords.sy, keypoint.y),
-//             sWidth: max(boxCoords.sWidth, keypoint.x),
-//             sHeight: max(boxCoords.sHeight, keypoint.y)
-//           }
-//         }
+        for (let i = 0; i < hand.keypoints.length; i++) {
+          let keypoint = hand.keypoints[i];
+          circle(keypoint.x, keypoint.y, 16);
+          boxCoords = {
+            sx: min(boxCoords.sx, keypoint.x),
+            sy: min(boxCoords.sy, keypoint.y),
+            sWidth: max(boxCoords.sWidth, keypoint.x),
+            sHeight: max(boxCoords.sHeight, keypoint.y)
+          }
+        }
 
-//         noFill();
-//         stroke(1);
-//         rect(boxCoords.sx, boxCoords.sy, boxCoords.sWidth - boxCoords.sx, boxCoords.sHeight - boxCoords.sy);
-//       }
-//       let handWidth = boxCoords.sWidth - boxCoords.sx + 40;
-//       let handHeight = boxCoords.sHeight - boxCoords.sy + 40;
-//       image(video, videoSize + boxCoords.sx, boxCoords.sy, handWidth, handHeight, boxCoords.sx - 20, boxCoords.sy - 20, handWidth, handHeight);
+        noFill();
+        stroke(1);
+        rect(boxCoords.sx, boxCoords.sy, boxCoords.sWidth - boxCoords.sx, boxCoords.sHeight - boxCoords.sy);
+      }
+      let handWidth = boxCoords.sWidth - boxCoords.sx + 40;
+      let handHeight = boxCoords.sHeight - boxCoords.sy + 40;
+      image(video, videoSize + boxCoords.sx, boxCoords.sy, handWidth, handHeight, boxCoords.sx - 20, boxCoords.sy - 20, handWidth, handHeight);
 
-//       boxCoords = {
-//         sx: videoSize,
-//         sy: videoSize,
-//         sWidth: 0,
-//         sHeight: 0
-//       }
-//     }
-//   }
+      boxCoords = {
+        sx: videoSize,
+        sy: videoSize,
+        sWidth: 0,
+        sHeight: 0
+      }
+    }
+  }
 
-//   textSize(64);
-//   textAlign(CENTER, CENTER);
-//   fill(255);
-//   text(label, width / 2, 3 / 4 * height);
-// }
+  textSize(64);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  text(label, width / 2, 3 / 4 * height);
+
+}
